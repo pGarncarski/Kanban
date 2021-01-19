c6ae065d6e378125a4a2def49c6379fb95eb8341
gancek77 <gancek77@gmail.com>
2021-01-19 20:16:38 +0100

    Added Edit and Display Templates, added ProjectStatsViewComponent

---
+++ Kanban/Common/Consts.cs
@@ -0,0 +1,18 @@
+﻿using Kanban.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kanban.Common
+{
+    public class Consts
+    {
+        public static Dictionary<IssueState, string> IssueStates { get; } = new Dictionary<IssueState, string>
+        {
+            {IssueState.Todo, "do zrobienia" },
+            {IssueState.Doing, "robione" },
+            {IssueState.Done, "zrobione" }
+        };
+    }
+}
---
+++ Kanban/Components/ProjectStatsViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using Kanban.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kanban.Components
+{
+    [ViewComponent]
+    public class ProjectStatsViewComponent : ViewComponent
+    {
+        private readonly KanbanContext _context;
+
+        public ProjectStatsViewComponent(KanbanContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var q =
+                from issue in _context.Issues
+                group issue by issue.State into issueStates
+                select new { IssueState = issueStates.Key, Count = issueStates.Count() };
+
+            var projectStats = await q.ToListAsync();
+            var projectStatsModel = projectStats.ToDictionary(stat => stat.IssueState, stat => stat.Count);
+
+            return View(projectStatsModel);
+        }
+    }
+}
